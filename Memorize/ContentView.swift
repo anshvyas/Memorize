05da5bcee455b649822fd4c326541bcc879a98c4
Anshul Vyas <anshul.vyas380@gmail.com>
2020-08-02 19:43:34 +0530

    Add a card with emoji

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 16.0).stroke(lineWidth: 2.0)
+            Text("👻").font(.largeTitle)
+        }
+        .padding()
+            //The foreground color doesn't make sense for a Zstack.
+            //So it is passed down to all the views inside it.
+            .foregroundColor(.orange)
     }
 }
 
+//MARK: This is for preview pane.
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
