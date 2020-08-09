4e0fb72ef1322255d93b10a662b92ebe137477e3
Anshul Vyas <anshul.vyas380@gmail.com>
2020-08-09 19:18:11 +0530

    Init game with random no of card pairs

--- Memorize/SceneDelegate.swift
+++ Memorize/SceneDelegate.swift
@@ -20,7 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView(emojiGameViewModel: EmojiMemoryGame(cardPairs: 3))
+        let cardPairs = Int.random(in: 2...5)
+        let contentView = ContentView(emojiGameViewModel: EmojiMemoryGame(cardPairs: cardPairs))
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
